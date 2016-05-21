f75445e717308208768865f49e1f5d4e10e78bba
Jason_Liu <jasonliuspark@gmail.com>
2016-05-21 16:32:46 +1000

    details page implement

--- alcoholMonitoring/app/src/main/java/project/alcoholmonitoring/alcohol_details/AlcoholDetailsTrackActivity.java
+++ alcoholMonitoring/app/src/main/java/project/alcoholmonitoring/alcohol_details/AlcoholDetailsTrackActivity.java
@@ -3,9 +3,12 @@ package project.alcoholmonitoring.alcohol_details;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.synnapps.carouselview.CarouselView;
@@ -18,8 +21,15 @@ import project.alcoholmonitoring.R;
  */
 public class AlcoholDetailsTrackActivity extends AppCompatActivity {
 
-    CarouselView carouselView;
 
+    // TODO: 21/05/16 alcohol volume buttons
+    CarouselView carouselView;
+    FloatingActionButton plus;
+    FloatingActionButton minus;
+    TextView quantity;
+    TextView volume;
+    TextView standard_drink;
+    SeekBar verticalBar;
     @Override
     public Context getApplicationContext() {
         return super.getApplicationContext();
@@ -30,27 +40,80 @@ public class AlcoholDetailsTrackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alcohol_details);
-
+        //init carousel
         carouselView = (CarouselView) findViewById(R.id.carouselView);
         carouselView.setPageCount(drinkImages.length);
-
         carouselView.setImageListener(imageListener);
+        //bind action buttons
+        plus=(FloatingActionButton)findViewById(R.id.DrinkPlus);
+        plus.setOnClickListener(plusB);
+        minus=(FloatingActionButton)findViewById(R.id.DrinkMinus);
+        minus.setOnClickListener(minusB);
+        //bind quantify text view
+        quantity=(TextView)findViewById(R.id.quantity);
+        //bind volume&standard text view
+        volume=(TextView)findViewById(R.id.volume);
+        standard_drink=(TextView)findViewById(R.id.standardDrinks);
+        //bind vertical bar
+        verticalBar=(SeekBar)findViewById(R.id.alcoholSlider);
+        verticalBar.setOnSeekBarChangeListener(seekBarChangeListener);
+
     }
 
+    SeekBar.OnSeekBarChangeListener seekBarChangeListener=new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            volume.setText(progress+"0");
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+    // carousel image listener
     ImageListener imageListener = new ImageListener() {
         @Override
         public void setImageForPosition(int position, ImageView imageView) {
             imageView.setImageResource(drinkImages[position]);
             imageView.setOnClickListener(imageClickListener);
+        }
+    };
+    //plus listener
+    FloatingActionButton.OnClickListener plusB= new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Integer temp=Integer.parseInt(quantity.getText().toString())+1;
+
+           quantity.setText(temp.toString());
+
+        }
+    };
+    //minus listener
+    FloatingActionButton.OnClickListener minusB=new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+
+            Integer temp=Integer.parseInt(quantity.getText().toString())-1;
+            if(temp >=0) {
+                quantity.setText(temp.toString());
+            }
+
 
         }
     };
 
+    //image click listener
     CarouselView.OnClickListener imageClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 //            carouselView.pauseCarousel();
-//
+//  // TODO: 21/05/16 click select alcohol
             Toast.makeText(getApplicationContext(), "clicked!",
                     Toast.LENGTH_LONG).show();
         }
