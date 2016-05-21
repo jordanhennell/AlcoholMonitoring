188156dd34dfa99c353f6d6e65ce1b9c870caf38
jordanhennell <jordan.hennell@hotmail.com>
2016-05-21 19:58:07 +1000

    Added material time picker for events

--- alcoholMonitoring/app/src/main/java/project/alcoholmonitoring/event/EventActivity.java
+++ alcoholMonitoring/app/src/main/java/project/alcoholmonitoring/event/EventActivity.java
@@ -1,6 +1,7 @@
 package project.alcoholmonitoring.event;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -9,31 +10,60 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 
 import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import project.alcoholmonitoring.R;
 
 public class EventActivity extends Activity {
 
+    TimePickerDialog tpd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_main);
 
+        showChosenDate();
         setUpReminderPopUp();
+        setUpTimePicker();
+    }
 
-        Intent intent = getIntent();
+    private void showChosenDate() {
+        Bundle extras = getIntent().getExtras();
 
-        Bundle extras = intent.getExtras();
         Date chosenDate = ((CalendarDay)extras.get("chosen_date")).getDate();
         String stringDate = String.format("%1$tY %1$tb %1$td", chosenDate);
+
         TextView dateTextView = (TextView)findViewById(R.id.eventDateDisplay);
         dateTextView.setText(stringDate);
+    }
+
+    private void setUpTimePicker() {
+        final Button chooseTimeButton = (Button)findViewById(R.id.eventChooseTimeButton);
+
+        final TimePickerDialog tpd = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+                        chooseTimeButton.setText(hourOfDay + ":" + minute);
+                    }
+                }, 12, 0, false);
+
+        chooseTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tpd.show();
+            }
+        });
     }
 
     private void setUpReminderPopUp() {
